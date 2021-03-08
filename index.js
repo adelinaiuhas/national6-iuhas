f1418dd25151c9ddc90df7a4e1d3ab5e91de96a8
adelinaiuhas <adelina.iuhas@gmail.com>
2021-03-08 22:15:31 +0200

    finished homework

--- index.js
+++ index.js
@@ -41,4 +41,47 @@ function renderArticle(articleData) {
 
     articleTitle.innerText = articleData.title;
     articleContent.innerText = articleData.content;
+
+    postId = articleData.id;
+    let urlData = "https://simple-json-server-scit.herokuapp.com/comments?postId=" + postId;
+    fetch(urlData)
+        .then(handleFetchResponses)
+        .then(useJSONResponses);
+
+    function handleFetchResponses(response) {
+        console.log("response", response);
+        return response.json();
+    }        
+
+    function useJSONResponses(json) {
+        console.log(json);
+        renderComments(json);
+    }    
+
+    function renderComments(commentsList) {
+        for (const commentsData of commentsList) {
+            console.log(commentsData);
+            renderComment(commentsData);
+        }
+    }
+    const containerComments = document.createElement("div");   
+    function  renderComment(commentsData) {
+     
+    const post = document.createElement("div");
+    const postUser = document.createElement("h4");
+    const postUserComment = document.createElement("p");
+
+    post.appendChild(postUser);
+    post.appendChild(postUserComment);
+    containerComments.appendChild(post);
+    article.appendChild(containerComments);
+    containerComments.className = "comments-list";
+    post.className = "comment";
+    post.style.paddingLeft = "20px";
+    postUser.className = "comment-user";
+    postUserComment.className = "comment-content";
+
+    postUser.innerText = commentsData.username;
+    postUserComment.innerText = commentsData.content;
+    } 
 }
