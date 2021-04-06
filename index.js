225f16d48df7e17f2aca5c5ee4c0944f1250f936
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-06 18:25:09 +0300

    first draft

--- index.js
+++ index.js
@@ -9,3 +9,35 @@ document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("password");
   window.location = "/";
 });
+
+const allBreeds = document.getElementById("breeds");
+window.addEventListener("load", getData);
+
+function getData() {
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then((response) => response.json())
+    .then((json) => useJSONResponse(json));
+}
+
+function useJSONResponse(json) {
+  var breedNamesObjects = Object.values(json);
+  console.log(breedNamesObjects[0]);
+  var breedNamesObject = breedNamesObjects[0];
+  var breedListArray = Object.keys(breedNamesObject);
+  console.log(breedListArray);
+  renderBreedNames(breedListArray);
+}
+
+function renderBreedNames(breedList) {
+
+  for (const breedName of breedList) {
+    console.log(breedName);
+    renderBreedName(breedName);
+  }
+}
+
+function renderBreedName(breedName) {
+  const breed = document.createElement("p");
+  allBreeds.appendChild(breed);
+  breed.innerHTML = breedName;
+}
