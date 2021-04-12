3a78f025c08039c9f9c2469bf2fa466239c72e1f
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-12 21:14:32 +0300

    prettier

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ fs.readFile("newFile.txt", 'utf8', function (err, data) {
     if (err) throw err;
     console.log('OK');
     console.log(data)
-  });
+});
 
-  const list1 = [1, 2, 3, 4];
-  const list2 = [3, 4, 5, 6];
-  const resultList = _.union(list1, list2);
-  console.log(resultList);
+const list1 = [1, 2, 3, 4];
+const list2 = [3, 4, 5, 6];
+const resultList = _.union(list1, list2);
+console.log(resultList);
