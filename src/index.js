51e7971f950ebb2f15a5b697b394361a697e181a
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-13 20:01:47 +0300

    api mod

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
+import { getToDoData, updateUserData, createUserData } from "./utils/API";
+
 console.log("To Do App");
+
+const ADD_NEW_USER_URL = "https://simple-json-server-scit.herokuapp.com/todo";
+const UPDATE_TO_USER_URL = "https://simple-json-server-scit.herokuapp.com/todo/aiuhas";
+const inputTask = document.getElementById("task-name");
+
+let todo = [];
+let userExist = true;
+
+getToDoData(() => {
+    console.log(json);
+    if (json.id === "aiuhas") {
+        todo = json.todo;
+        userExist = true;
+    }
+});
+
+document.getElementById("add-task-button").addEventListener("click", () => {
+    if (userExist) {
+        // update user data
+        console.log("add task to todo list");
+        const itemValue = inputTask.value;
+        if (itemValue) {
+            console.log(todo);
+            todo.push({
+                checked: false,
+                item: itemValue,
+            });
+            updateUserData(todo, () => { });
+        }
+    } else {
+        // update user to server
+        const itemValue = inputTask.value;
+        if (itemValue) {
+            createUserData(itemValue, () => { });
+        }
+    }
+});
