843d4b4f9a0ee616befcfc0b04f77b04f5af1192
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-15 21:02:01 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { AppLogo } from "../AppLogo/AppLogo";
+import "./Header.css";
+
+export function Header() {
+  return (
+    <div className="app-header">
+      {" "}
+      <AppLogo />{" "}
+    </div>
+  );
+}
