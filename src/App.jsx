c0cfb9cfa15aa2c2f23b4ed6592006216f656670
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-23 10:50:30 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header/Header";
-
-import './App.css';
+import { menuLogo } from "./components/Header/Menu/MenuButton/MenuButton";
+import "./App.css";
 
 function App() {
   return (
     <div className="App" id="app">
       <Header />
+      <menuLogo />
     </div>
   );
 }
---
+++ src/components/Header/Menu/MenuButton/MenuButton.jsx
@@ -0,0 +1,9 @@
+import menuSvg from "./menu.svg";
+//import "./AppLogo.css";
+export function menuLogo() {
+  return (
+    <div className="app-menu-button">
+      <img src={menuSvg} alt="button" />
+    </div>
+  );
+}
