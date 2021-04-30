3f5ca899ae2145f0ce3364c41391fb51ac7f54c7
adelinaiuhas <adelina.iuhas@gmail.com>
2021-04-30 11:58:22 +0300

    finished homework

--- src/components/Form/SendButton/SendButton.jsx
+++ src/components/Form/SendButton/SendButton.jsx
@@ -1,10 +1,10 @@
 import { Component } from "react";
+
 import "./SendButton.css";
 
 export class SendButton extends Component {
   handleOnClick = () => {
     const newErrors = [];
-
     const allKeys = ["firstName", "lastName", "email", "message"];
     allKeys.forEach((key) => {
       if (!this.props.formData[key]) {
@@ -13,6 +13,7 @@ export class SendButton extends Component {
     });
     this.props.onHandleErrors(newErrors);
   };
+
   render() {
     return (
       <div className="send-container">
